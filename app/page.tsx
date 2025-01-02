72033ea0dc75bea5aa19243ec6e2750b1f0c4063
Lucas Martins <100973838+lucasrmartins0@users.noreply.github.com>
2025-01-02 19:37:45 +0000

    home page +-

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
 export default function page() {
   return (
-    <div>
-      <p className='text-center'>Welcome to DEISI SHOP</p>
+    <div className="mb-200 relative flex items-center justify-center min-h-screen overflow-hidden">
+      <div />
+      <h1 className="relative text-black text-5xl md:text-7xl font-bold animate-bounce">
+        WELCOME TO DEISI SHOP
+      </h1>
     </div>
-  )
+  );
 }
